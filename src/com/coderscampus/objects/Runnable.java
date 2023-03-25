6709d64043880278a236a376bcf7537b1bf74b74
Knowlton2023 <122704053+Knowlton2023@users.noreply.github.com>
2023-03-25 16:33:03 -0500

    Updates v1

--- src/com/coderscampus/objects/Runnable.java
+++ src/com/coderscampus/objects/Runnable.java
@@ -4,13 +4,14 @@ public class Runnable {
 
 	public static void main(String[] args) {
 
-		Number number1 = new Number();
-
-		number1.number = 55;
+//		Number number1 = new Number();
+//
+//		number1.number = 55;
+//		
+//		System.out.println(number1.getNumber());
 		
-		System.out.println(number1.getNumber());
+		Scanner scanner = new Scanner(system.in);
 		
-	
 	
 	}
 
